6c2d2345aa2237638f5f6e0776cba93ac1462bda
LEMM <lmorenom2021@cic.ipn.mx>
2022-10-19 20:25:51 -0500

    EJERCICIO BITMAP

--- app/src/main/java/mx/ipn/cic/geo/bitmaps_corutinas/MainActivity.kt
+++ app/src/main/java/mx/ipn/cic/geo/bitmaps_corutinas/MainActivity.kt
@@ -1,15 +1,19 @@
 package mx.ipn.cic.geo.bitmaps_corutinas
 
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import mx.ipn.cic.geo.bitmaps_corutinas.databinding.ActivityMainBinding
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
    // Atributo para acceder a los widgets mediante vinculación de vista.
    private lateinit var binding: ActivityMainBinding
+   private lateinit var  imageViewShow: ImageView
 
    override fun onCreate(savedInstanceState: Bundle?) {
       super.onCreate(savedInstanceState)
@@ -17,6 +21,16 @@ class MainActivity : AppCompatActivity() {
       // setContentView(R.layout.activity_main)
       initBindingView()
       initButtonListener()
+      imageViewShow = findViewById(R.id.imagen)
+   }
+
+   private fun cargarBitmap(){
+      val aleatorio = (0..9).random()
+      val resourceId = resources.getIdentifier(
+         "imagen${aleatorio.toString()}", "drawable", packageName)
+      val task = BitmapEjercicio(imageViewShow)
+      task.execute(resourceId)
+      //return true
    }
 
    private fun initBindingView() {
@@ -32,9 +46,9 @@ class MainActivity : AppCompatActivity() {
       // Recuperar la referencia al widget usando vinculación de vistas.
       binding.btnCorutina.setOnClickListener {
          MainScope().launch {
-            binding.mensajeCorutina.text = resources.getText(R.string.mensaje_corutina_inicio)
-            delay(2000)
-            binding.mensajeCorutina.text = resources.getText(R.string.mensaje_corutina_fin)
+            Toast.makeText(applicationContext, "Cargando Imagen...", Toast.LENGTH_SHORT).show()
+               cargarBitmap()
+
          }
       }
    }
